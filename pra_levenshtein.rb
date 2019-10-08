1dbf9f3133a28cc2548dfe93e3d9eb3370084dc3
Taketo Igai <t.igai.716@gmail.com>
2019-10-08 15:15:01 +0900

    add comment

--- pra_levenshtein.rb
+++ pra_levenshtein.rb
@@ -5,11 +5,8 @@ module Levenshtein
     1 - normalized_distance(str1, str2)
   end
 
-# p Levenshtein.similarity("アサヒ スーパードライ エクストラコールド　生ビール") #=> 0.6666666666666667
-# p Levenshtein.similarity("ザ・プレミアム・モルツ") #=> 0.6
-# p Levenshtein.similarity("サッポロ黒ラベル　ジョッキ") #=> 0.0
-
   def self.check_ebisu(drink)
+    #ヱビスは'ビ'が入っているためポイントが高くなりガチなので丁寧に
     if drink.include?("ヱビス") || drink.include?("エビス")
       p "エビスビール"
     elsif drink.include?('生ビール')
@@ -19,27 +16,30 @@ module Levenshtein
     end
   end
 
-  def self.beer_comparer(beer_name)
-      kirin = Levenshtein.similarity(beer_name, "キリン一番搾り")
-      asahi = Levenshtein.similarity(beer_name, "アサヒスーパードライ")
-      puremoru = Levenshtein.similarity(beer_name, "ザ・プレミアム・モルツ")
-      sapporo = Levenshtein.similarity(beer_name, "サッポロ黒ラベル")
-      ebisu =  Levenshtein.similarity(beer_name, "ヱビスビール")
-      beer_array = [kirin, asahi, puremoru, sapporo, ebisu]
-      beer_array.sort!.reverse!
-      p beer_array[0]
-      if beer_array[0] <= 0.15
-        p "その他"
-      elsif beer_array[0] == kirin
-        p "キリン"
-      elsif beer_array[0] == asahi
-        p "アサヒスーパードライ"
-      elsif beer_array[0] == puremoru
-        p "ザ・プレミアム・モルツ"
-      elsif beer_array[0] == sapporo
-        p "サッポロ黒ラベル"
-      else
-        Levenshtein.check_ebisu(beer_name)
-      end
+  def self.beer_comparer(beer_name
+    それぞれの類似ポイントを算出
+    kirin = Levenshtein.similarity(beer_name, "キリン一番搾り")
+    asahi = Levenshtein.similarity(beer_name, "アサヒスーパードライ")
+    puremoru = Levenshtein.similarity(beer_name, "ザ・プレミアム・モルツ")
+    sapporo = Levenshtein.similarity(beer_name, "サッポロ黒ラベル")
+    ebisu =  Levenshtein.similarity(beer_name, "ヱビスビール")
+    beer_array = [kirin, asahi, puremoru, sapporo, ebisu]
+    #点数順に並べる
+    beer_array.sort!.reverse!
+    p beer_array[0]
+    #ポイントが一番高いものを選別
+    if beer_array[0] <= 0.15 #ここのポイントは微調整が必要
+      p "その他"
+    elsif beer_array[0] == kirin
+      p "キリン"
+    elsif beer_array[0] == asahi
+      p "アサヒスーパードライ"
+    elsif beer_array[0] == puremoru
+      p "ザ・プレミアム・モルツ"
+    elsif beer_array[0] == sapporo
+      p "サッポロ黒ラベル"
+    else
+      Levenshtein.check_ebisu(beer_name)
+    end
   end
 end
